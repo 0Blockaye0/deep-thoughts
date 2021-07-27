ed022838c1a00f9e7f2ce55454ac666a6322d7d7
Blake Johnson <blakejohnson@Blakes-MacBook-Pro.local>
2021-07-27 13:45:06 -0400

    concurrently running both server and client

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
-const express = require('express');
+const path = require("path");
+const express = require("express");
 // import ApolloServer
-const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
+const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth");
 
 // import our typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -16,25 +17,35 @@ async function startServer() {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: authMiddleware
+    context: authMiddleware,
   });
   await server.start();
   server.applyMiddleware({ app });
 
   app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+  app.use(express.json());
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    // log where we can go to test our GQL API
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
+  // Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      // log where we can go to test our GQL API
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
 }
 
 startServer();
 
 // // integrate our Apollo server with the Express application as middleware
 // server.applyMiddleware({ app });
-
